448557943184ce08cbf38a1e02f6a3a540726de6
Your Name <davidnielsen215@gmail.com>
2019-04-17 17:19:01 -0600

    added daily-planet to projects

---
+++ exercises/Conventions/app.js
@@ -0,0 +1,32 @@
+//camelCase
+var myVariableIsNamedVar = 'this is my variable'
+
+//Whitespace
+
+// var myVar = 'stuff'
+// if(myVar==='things'){console.log('cool')}else{console.log('dumg')}
+
+var pretty5um = 4 * 3 / 2 + (5 - 1)
+var person = {
+    name: 'Bob', 
+    age: 32
+    }
+    if (2 < 3){
+        doThing()
+    } else{
+        doAnotherThing()
+    }
+
+    var isMale = true
+    var numbers = [1,2,3,4,5,6]
+    var animals = ['dog','cat']
+    var hobbies = ['fishing','climbing','swimming']
+    var people = [{
+        name: 'stew',
+        age: 100,
+        name: 'matt',
+        age: 100,
+        name: 'pat',
+        age: 100,
+    }]
+    
